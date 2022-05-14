6b1f71a2d1008d6d6a972b7b8f5b964b753e1728
Fox Alice <elviratr@yandex.ru>
2022-05-14 17:52:26 +0300

    feat: open/close popup

--- sripts/index.js
+++ sripts/index.js
@@ -1,1 +1,20 @@
-console.log('Hello, world!');
+//console.log('Hello, world!');
+
+const openButton = document.querySelector('.profile__button');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__close-icon');
+
+console.log({openButton, 
+            popup, 
+            closeButton});
+
+function togglePopup() {
+    popup.classList.toggle('popup_opened')
+}
+openButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
+
+popup.addEventListener('click', function(event){
+    if (event.target === event.currentTarget)
+    togglePopup();
+})
