b3545bd55d98b0c78abc662f050efbf304f18e6a
Fox Alice <elviratr@yandex.ru>
2022-05-17 21:46:18 +0300

    feat: fix mistakes js

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,6 @@ let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__form-item_type_name');
 let jobInput = document.querySelector('.popup__form-item_type_job');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    nameInput.value;
-    jobInput.value;
-    let Name = document.querySelector('.profile__title');
-    let Job = document.querySelector('.profile__description');
-    Name.textContent = nameInput.value;
-    Job.textContent = jobInput.value;
-}
-
 formElement.addEventListener('submit', formSubmitHandler);
 
 const openButton = document.querySelector('.profile__button');
@@ -22,10 +12,19 @@ const closeButton = document.querySelector('.popup__close-icon');
 
 function togglePopup() {
     popup.classList.toggle('popup_opened');
-    let Name = document.querySelector('.profile__title');
-    let Job = document.querySelector('.profile__description');
-    nameInput.value = Name.textContent;
-    jobInput.value = Job.textContent;
+    let nameProfile = document.querySelector('.profile__title');
+    let jobProfile = document.querySelector('.profile__description');
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
 }
 openButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    if (popup.classList.contains('popup_opened')) { 
+    let nameProfile = document.querySelector('.profile__title');
+    let jobProfile = document.querySelector('.profile__description');
+    nameProfile.textContent = nameInput.value;
+    jobProfile.textContent = jobInput.value;
+    }    
+}
