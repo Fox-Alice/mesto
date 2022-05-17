fd7d4f7a3e9cf54170d68f6bbe849120dcb55c4c
Fox Alice <elviratr@yandex.ru>
2022-05-17 13:15:21 +0300

    style: fix some mistakes

--- sripts/index.js
+++ sripts/index.js
@@ -1,20 +1,31 @@
-//console.log('Hello, world!');
+
+let formElement = document.querySelector('.popup__form');
+
+let nameInput = document.querySelector('.popup__form-item_type_name');
+let jobInput = document.querySelector('.popup__form-item_type_job');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    nameInput.value;
+    jobInput.value;
+    let Name = document.querySelector('.profile__title');
+    let Job = document.querySelector('.profile__description');
+    Name.textContent = nameInput.value;
+    Job.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
 
 const openButton = document.querySelector('.profile__button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-icon');
 
-console.log({openButton, 
-            popup, 
-            closeButton});
-
 function togglePopup() {
-    popup.classList.toggle('popup_opened')
+    popup.classList.toggle('popup_opened');
+    let Name = document.querySelector('.profile__title');
+    let Job = document.querySelector('.profile__description');
+    nameInput.value = Name.textContent;
+    jobInput.value = Job.textContent;
 }
 openButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
-
-popup.addEventListener('click', function(event){
-    if (event.target === event.currentTarget)
-    togglePopup();
-})
