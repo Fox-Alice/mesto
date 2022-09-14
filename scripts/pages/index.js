4153afbb2d105a065a18156d81843e73a3681f26
Fox Alice <elviratr@yandex.ru>
2022-09-14 23:36:32 +0300

    feat: make gitignore

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,9 +1,9 @@
-import Card from "./Card.js";
-import FormValidator from "./FormValidator.js";
-import Section from "./Section.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
 
 const selectors = {
   cardTitle: 'card__title',
@@ -43,7 +43,6 @@ const popupEditInfo = new PopupWithForm('.edit-popup', fillFormEditProfile);
 popupEditInfo.setEventListeners();
 
 const userInfo = new UserInfo('.profile__title', '.profile__description');
-console.log(userInfo.getUserInfo());
 userInfo.setUserInfo(userInfo.getUserInfo()); 
 
 const formEditValidator = new FormValidator(validationConfig, editForm);
